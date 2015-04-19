9cdb456db412aee8d0c6798367135c51f2befabf
leeter <leeterfp@gmail.com>
2015-04-19 10:39:19 -0600

    more changes

--- leeterchat/mainwindow.cpp
+++ leeterchat/mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+#include <QDateTime>
 #include "mainwindow.hpp"
 #include "ui_mainwindow.h"
 
@@ -11,4 +13,29 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_textInput_returnPressed()
+{
+    if(ui->textInput->displayText().isEmpty())
+    {
+        return;
+    }
+    QTableWidget * textOutput = ui->textOutput;
+    int targetRow = textOutput->rowCount();
+    textOutput->insertRow(textOutput->rowCount());
+
+    std::unique_ptr<QTableWidgetItem> timeStamp(
+                new QTableWidgetItem(QDateTime::currentDateTime().toString(QLatin1String("HH:mm:ss"))));
+    std::unique_ptr<QTableWidgetItem> nick(
+                new QTableWidgetItem(QString::fromUtf8("Leeter")));
+    nick->setTextAlignment(Qt::AlignRight);
+    std::unique_ptr<QTableWidgetItem> text(
+                new QTableWidgetItem(ui->textInput->displayText()));
+
+    textOutput->setItem(targetRow, 0, timeStamp.release());
+    textOutput->setItem(targetRow, 1, nick.release());
+    textOutput->setItem(targetRow, 2, text.release());
+    textOutput->scrollToBottom();
+    ui->textInput->clear();
 }
--- leeterchat/mainwindow.hpp
+++ leeterchat/mainwindow.hpp
@@ -1,6 +1,5 @@
 #ifndef MAINWINDOW_HPP
 #define MAINWINDOW_HPP
-
 #include <QMainWindow>
 
 namespace Ui {
@@ -12,11 +11,14 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_textInput_returnPressed();
+
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow * ui;
 };
 
 #endif // MAINWINDOW_HPP
